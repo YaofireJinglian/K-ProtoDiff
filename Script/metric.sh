a77be5c01339b3146c07c708cb89b16cde1d670f
JingLianYaoFire <57284806+YaofireJinglian@users.noreply.github.com>
2025-12-02 15:25:48 +0800

    Delete Script directory

--- Script/metric.sh
+++
@@ -1,4 +0,0 @@
-
-python metric_pytorch.py --root "$root$cfg" \
---ori_path "$root/samples/${cfg}_norm_truth_24_train.npy" \
---fake_path "$root$name/ddpm_fake_${name}.npy"
--- Script/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-names=("EEG" "Stocks" "Exchange" "ETTh" "Electricity" "Energy" "Traffic" "Weather" "Illness" )
-for name in "${names[@]}"; do
-    python run.py --name $name --config_file ./Config/$name.yaml --gpu 3 --train
-    python run.py --name $name --config_file ./Config/$name.yaml --gpu 3 --sample 0 --milestone 10
-done
